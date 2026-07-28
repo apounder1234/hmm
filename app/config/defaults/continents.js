b58b1de89db2c16f545603dfb4d7f95b5f922edc
apounder1234 <pounderandrew@gmail.com>
2026-07-28 09:34:43 +0200

    Delete app directory

--- app/config/defaults/continents.js
+++
@@ -1,9 +0,0 @@
-export const continents = [
-    { id: "africa", name: "Africa", printedResources: { fossilFuel: 10, biomass: 10, constructionMaterials: 8, criticalMaterials: 12 }, opportunities: { solar: 5, wind: 4, hydro: 4, biomass: 4, fossil: 3 }, startingKnowledge: 1 },
-    { id: "asia", name: "Asia", printedResources: { fossilFuel: 12, biomass: 10, constructionMaterials: 10, criticalMaterials: 10 }, opportunities: { solar: 4, wind: 4, hydro: 5, biomass: 4, fossil: 5 }, startingKnowledge: 2 },
-    { id: "europe", name: "Europe", printedResources: { fossilFuel: 6, biomass: 8, constructionMaterials: 14, criticalMaterials: 8 }, opportunities: { solar: 2, wind: 5, hydro: 3, biomass: 3, fossil: 2 }, startingKnowledge: 2 },
-    { id: "northAmerica", name: "North America", printedResources: { fossilFuel: 12, biomass: 10, constructionMaterials: 10, criticalMaterials: 8 }, opportunities: { solar: 4, wind: 5, hydro: 4, biomass: 4, fossil: 5 }, startingKnowledge: 2 },
-    { id: "southAmerica", name: "South America", printedResources: { fossilFuel: 8, biomass: 12, constructionMaterials: 8, criticalMaterials: 14 }, opportunities: { solar: 4, wind: 4, hydro: 5, biomass: 5, fossil: 3 }, startingKnowledge: 2 },
-    { id: "australia", name: "Australia", printedResources: { fossilFuel: 10, biomass: 6, constructionMaterials: 10, criticalMaterials: 14 }, opportunities: { solar: 5, wind: 4, hydro: 2, biomass: 2, fossil: 4 }, startingKnowledge: 2 }
-];
-//# sourceMappingURL=continents.js.map
--- app/simulation/exporters/json.js
+++
@@ -1,4 +0,0 @@
-export function simulationReportToJson(report, pretty = true) {
-    return JSON.stringify(report, null, pretty ? 2 : 0);
-}
-//# sourceMappingURL=json.js.map
